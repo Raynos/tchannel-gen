589ecea736ed3aef6df02d8759f6d0cd3cb98a20
Raynos <raynos2@gmail.com>
2015-06-21 22:48:27 -0700

    copy over an empty template

---
+++ template/content/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {{projectName}};
+
+function {{projectName}}() {
+    throw new Error('Not Implemented');
+}
---
+++ template/index.js
@@ -0,0 +1,64 @@
+var spawn = require('child_process').spawn;
+var path = require('path');
+var fs = require('fs');
+var prompt = require('promptly').prompt;
+var camelize = require('camelize');
+var format = require('util').format;
+
+function fetchFromGitConfig(key) {
+    function readValue(values, callback) {
+        var called = false;
+        var proc = spawn('git', [
+            '--bare',
+            'config',
+            '--global',
+            key
+        ]);
+
+        proc.stdout.once('data', function (chunk) {
+            called = true;
+            callback(null, String(chunk));
+        });
+        proc.stdout.once('error', callback);
+        proc.stdout.once('end', function () {
+            if (called) {
+                return;
+            }
+
+            var message = format('please configure %s in git', key);
+            callback(new Error(message));
+        });
+    }
+
+    return readValue;
+}
+
+module.exports = {
+    project: 'Project name: ',
+    description: function (values, callback) {
+        var pkg = path.join(values.project, 'package.json');
+
+        fs.readFile(pkg, function (err, buf) {
+            if (err) return promptFor();
+
+            var json = JSON.parse(String(buf));
+            if (!json.description) {
+                return promptFor();
+            }
+
+            callback(null, json.description);
+        });
+
+        function promptFor() {
+            prompt('  Project description: ', callback);
+        }
+    },
+    gitName: fetchFromGitConfig('user.name'),
+    email: fetchFromGitConfig('user.email'),
+    projectNoDash: function readProjectNoDash(values, cb) {
+        cb(null, values.project.replace(/\-/g, ''));
+    },
+    projectName: function readProjectName(values, callback) {
+        callback(null, camelize(values.project));
+    }
+};
