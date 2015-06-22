3e22d6da87a5df2ae9aade79d569dfe1e377d412
Raynos <raynos2@gmail.com>
2015-06-21 22:47:55 -0700

    scaffold stuff

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 'use strict';
 
-module.exports = tchannelGen;
+var process = require('process');
+var ngen = require('uber-ngen/bin/ngen.js');
+var parseArgs = require('minimist');
 
-function tchannelGen() {
+if (require.main === module) {
+    main(parseArgs(process.argv.slice(2)));
+}
 
+function main(opts) {
+    opts.template = 'template';
+    opts.directory = __dirname;
+
+    ngen(opts);
 }
