ed176d8f4d0dd9bad38a902e577129b45e40330d
Raynos <raynos2@gmail.com>
2015-06-24 15:29:34 -0700

    server: print extra details in startup

--- template/content/server.js
+++ template/content/server.js
@@ -1,26 +1,43 @@
 #!/usr/bin/env node
 'use strict';
 
+var fileStart = Date.now();
+
+var hostname = require('os').hostname;
 var process = require('process');
+var path = require('path');
+var getRepoInfo = require('git-repo-info');
+var parseArgs = require('minimist');
 
 var Application = require('./app.js');
 
 module.exports = main;
 
-/*istanbul ignore else*/
+/*istanbul ignore else: dead branch*/
 if (require.main === module) {
-    main();
+    main({
+        argv: parseArgs(process.argv.slice(2))
+    });
 }
 
-function main() {
+function main(opts) {
+    var mainStart = Date.now();
+
     /*eslint no-process-exit: 0*/
     var app = Application();
 
     // attach before throwing exception
     process.on('uncaughtException', app.clients.uncaught);
 
-    app.bootstrap(function onAppReady(err) {
-        /*istanbul ignore if*/
+    var gitRepo = path.join(__dirname, '.git');
+    var gitSha = getRepoInfo(gitRepo).sha;
+
+    app.bootstrap(onAppReady);
+
+    var bootstrapEnd = Date.now();
+
+    function onAppReady(err) {
+        /*istanbul ignore if: should never happen in prod*/
         if (err) {
             app.clients.logger.fatal('Could not start', {
                 error: err
@@ -30,8 +47,16 @@ function main() {
 
         // TODO set process.title
 
+        var now = Date.now();
         app.clients.logger.info('server started', {
-            serverAddress: app.clients.rootChannel.address()
+            serverAddress: app.clients.rootChannel.address(),
+            gitSha: gitSha,
+            startupTiming: {
+                beforeMain: mainStart - fileStart,
+                appTime: bootstrapEnd - mainStart,
+                startTime: now - bootstrapEnd,
+                totalTime: now - fileStart
+            }
         });
     });
 }
